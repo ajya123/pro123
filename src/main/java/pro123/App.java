c5ea987d71c7491d7dad22fb1ae6ef28886c0d98
Rasool93980 <89054649+Rasool93980@users.noreply.github.com>
2021-09-12 10:25:33 +0530

    Update App.java

--- src/main/java/pro123/App.java
+++ src/main/java/pro123/App.java
@@ -1,19 +1,8 @@
 package pro123;
-import java.util.Scanner;
-
-public class App {
-
-    public static void main(String[] args) {
-
-        // Creates a reader instance which takes
-        // input from standard input - keyboard
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-
-        // nextInt() reads the next integer from the keyboard
-        int number = reader.nextInt();
-
-        // println() prints the following line to the output screen
-        System.out.println("You entered: " + number);
-    }
+ public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    System.out.println(x + y);
+  }
 }
